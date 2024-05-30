5d97c04daf165e41435f3031c8fd28374af9ffba
Danny castrillon <dannycastrillon2@hotmail.com>
2024-05-30 00:36:18 -0500

    multas_migracion

--- database/migrations/2024_05_30_052842_create_multas_table.php
+++ database/migrations/2024_05_30_052842_create_multas_table.php
@@ -1,27 +1,28 @@
 <?php
 
+// database/migrations/xxxx_xx_xx_xxxxxx_create_multas_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMultasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('multas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->decimal('monto', 8, 2);
+            $table->text('motivo');
             $table->timestamps();
+
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('multas');
     }
-};
+}
+

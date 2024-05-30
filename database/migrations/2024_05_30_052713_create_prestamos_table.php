e3dc963e693f3cabac449486cdd34c2d599366a6
Danny castrillon <dannycastrillon2@hotmail.com>
2024-05-30 00:33:47 -0500

    prestamos_migracion

--- database/migrations/2024_05_30_052713_create_prestamos_table.php
+++ database/migrations/2024_05_30_052713_create_prestamos_table.php
@@ -1,27 +1,29 @@
 <?php
 
+// database/migrations/xxxx_xx_xx_xxxxxx_create_prestamos_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePrestamosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('prestamos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('libro_id');
+            $table->unsignedBigInteger('usuario_id');
+            $table->date('fecha_prestamo');
+            $table->date('fecha_devolucion')->nullable();
             $table->timestamps();
+
+            $table->foreign('libro_id')->references('id')->on('libros')->onDelete('cascade');
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('prestamos');
     }
-};
+}

2d80a92c3e51c465bdf459c54622bde6a4c176f9
Danny castrillon <dannycastrillon2@hotmail.com>
2024-05-30 01:26:05 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LibroController;
+use App\Http\Controllers\Api\PrestamoController;
+use App\Http\Controllers\Api\UsuarioController;
+use App\Http\Controllers\Api\EventoController;
+use App\Http\Controllers\Api\MultaController;
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::resource('libros', LibroController::class);
+    Route::resource('prestamos', PrestamoController::class);
+    Route::resource('usuarios', UsuarioController::class);
+    Route::resource('eventos', EventoController::class);
+    Route::resource('multas', MultaController::class);
 });
+

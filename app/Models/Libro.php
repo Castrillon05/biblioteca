e9eee92f325330638eb663e7299f1d1f926743be
Danny castrillon <dannycastrillon2@hotmail.com>
2024-05-30 00:51:56 -0500

    libro_model

---
+++ app/Models/Libro.php
@@ -0,0 +1,13 @@
+<?php
+
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Libro extends Model
+{
+    protected $fillable = ['titulo', 'autor', 'isbn', 'fecha_publicacion', 'cantidad_disponible'];
+}
+

c84dfcec547d98b07acaf3369a77dd2f983b8ee8
Danny castrillon <dannycastrillon2@hotmail.com>
2024-05-30 00:33:08 -0500

    libros_migracion

--- database/migrations/2024_05_30_052525_create_libros_table.php
+++ database/migrations/2024_05_30_052525_create_libros_table.php
@@ -1,27 +1,27 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateLibrosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('libros', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('isbn')->unique();
+            $table->date('fecha_publicacion');
+            $table->integer('cantidad_disponible');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('libros');
     }
-};
+}

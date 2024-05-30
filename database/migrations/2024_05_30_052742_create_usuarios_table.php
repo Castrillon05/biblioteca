f2168e476deaafedb33b078fd7c47b566a3d1611
Danny castrillon <dannycastrillon2@hotmail.com>
2024-05-30 00:34:40 -0500

    usuarios_migracion

--- database/migrations/2024_05_30_052742_create_usuarios_table.php
+++ database/migrations/2024_05_30_052742_create_usuarios_table.php
@@ -1,27 +1,26 @@
 <?php
 
+// database/migrations/xxxx_xx_xx_xxxxxx_create_usuarios_table.php
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsuariosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('usuarios');
     }
-};
+}
+
